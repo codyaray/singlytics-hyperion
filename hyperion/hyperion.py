c35bd6c94466948b9941265fd67adaa6b6f0db1b
Matt Kemp <mattkemp@gmail.com>
2012-09-29 11:58:26 -0500

    Setting up heroku

--- hyperion/hyperion.py
+++ hyperion/hyperion.py
@@ -74,4 +74,5 @@ def hyperion_profile_retrieval(application, account):
   return Response(status=400)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
