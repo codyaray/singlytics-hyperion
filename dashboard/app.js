6d8501ea2fb3ebd39496dead8706f2400a2d22e7
Alex Zelenskiy <zelenskiy.alex@gmail.com>
2012-09-29 14:07:04 -0500

    beginning of hyperion library

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,8 +1,26 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    hype = require('./glue/hype').hype,
+    app = express();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views')
+  app.set('view engine', 'jade')
+  app.use(express.bodyParser())
+  app.use(express.methodOverride())
+  app.use(app.router)
+  app.use(express.static(__dirname + '/public'))
+  app.set('view options', { layout: false })
+})
 
 app.get("/", function (req, res) {
-  res.send("app is running!")
+  res.render('index', {title: "main"})
+})
+
+app.get("/application/:id", function (req, res) {
+  var appId = req.params.id
+  hype.getApp(appId, function (data) {
+    res.render('application', {title: "application", appId: req.params.id, data: data})
+  })
 })
 
 app.listen(3000)
