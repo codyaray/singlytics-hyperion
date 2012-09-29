a982545f6bbf66c2a8c0d64bf70c1e0ce14aaf7f
Leif Kramer <lkramer@brighttag.com>
2012-09-29 12:57:23 -0500

    get stub up

---
+++ adnet/routes/ads.js
@@ -0,0 +1,3 @@
+exports.ads = function(req, res){
+  res.send('Hello World');
+};
--- adnet/routes/index.js
+++ adnet/routes/index.js
@@ -4,15 +4,5 @@
  */
 
 exports.index = function(req, res){
-
-res.set({
-  'status' : 200;
-  'Content-Type': 'text/plain',
-  'Content-Length': '123',
-  'X-URL': 'http://pathtojpeg.com/jpeg';
-  'X-href' : 'http://pathtolink.com';
-  'uniqueID' : 'uniqueID';
-  'appID' : '';
-});
-
+  res.send('{"image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-MediumRectangle-300x250.jpg","URL":"http://www.singly.com"}');
 };
